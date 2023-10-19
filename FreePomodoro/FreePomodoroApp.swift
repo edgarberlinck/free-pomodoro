8ec56fb0725d330f1115f7b98c60bbcda8e3669b
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-10-19 18:32:02 +0200

    Initial Commit

---
+++ FreePomodoro/FreePomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreePomodoroApp.swift
+//  FreePomodoro
+//
+//  Created by Edgar Muniz Berlinck on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct FreePomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
